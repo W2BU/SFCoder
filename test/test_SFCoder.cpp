9709f8f677eba6a10df297fb95bba110d8078de6
W2BU <75771989+W2BU@users.noreply.github.com>
2022-05-11 03:04:42 +0300

    hotfix

--- test/test_SFCoder.cpp
+++ test/test_SFCoder.cpp
@@ -6,5 +6,33 @@ TEST(SFCoder, oneLetter)
     std::string text = "a";
     SFCoder mycoder(text);
 
+    EXPECT_EQ(mycoder.compression_ratio(), 0);
+    EXPECT_EQ(mycoder.get_encoded(), "");
+    EXPECT_EQ(mycoder.get_decoded(), text);
+    EXPECT_EQ(mycoder.get_orsize(), 8);
+    EXPECT_EQ(mycoder.get_ensize(), 0);
+}
 
+TEST(SFCoder, twoLetters)
+{
+    std::string text = "ab";
+    SFCoder mycoder(text);
+
+    EXPECT_EQ(mycoder.compression_ratio(), 0.125);
+    EXPECT_EQ(mycoder.get_encoded(), "01");
+    EXPECT_EQ(mycoder.get_decoded(), text);
+    EXPECT_EQ(mycoder.get_orsize(), 16);
+    EXPECT_EQ(mycoder.get_ensize(), 2);
+}
+
+TEST(SFCoder, smallPhrase)
+{
+    std::string text = "I'll be back";
+    SFCoder mycoder(text);
+
+    EXPECT_EQ(mycoder.compression_ratio(), (38 * 1.f/96));
+    EXPECT_EQ(mycoder.get_encoded(), "10011110010010100010000100011001101101");
+    EXPECT_EQ(mycoder.get_decoded(), text);
+    EXPECT_EQ(mycoder.get_orsize(), 96);
+    EXPECT_EQ(mycoder.get_ensize(), 38);
 }
